a9a780e2c5e95430505f38f35adcdd2b3b31aa6f
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-06-08 10:03:24 +0200

    Adds compareTo(...)

--- src/main/java/com/github/verhagen/mrrs/domain/Facility.java
+++ src/main/java/com/github/verhagen/mrrs/domain/Facility.java
@@ -1,6 +1,6 @@
 package com.github.verhagen.mrrs.domain;
 
-public class Facility {
+public class Facility implements Comparable<Facility>{
 	private final String name;
 
 
@@ -11,6 +11,12 @@ public class Facility {
 
 	public String getName() {
 		return name;
+	}
+
+
+	@Override
+	public int compareTo(final Facility other) {
+		return this.getName().compareTo(other.getName());
 	}
 
 }
--- src/test/java/com/github/verhagen/mrrs/domain/FacilityTest.java
+++ src/test/java/com/github/verhagen/mrrs/domain/FacilityTest.java
@@ -1,10 +1,12 @@
 package com.github.verhagen.mrrs.domain;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class FacilityTest {
+
 
 	@Test
 	public void createComputerWithLargeScreen() throws Exception {
@@ -19,6 +21,14 @@ public class FacilityTest {
 		Facility facility = new Facility("Whiteboard");
 		
 		assertEquals("Whiteboard", facility.getName());
+	}
+	
+	@Test
+	public void compare() throws Exception {
+		Facility whiteboard = new Facility("whiteboard");
+		Facility beamer = new Facility("beamer");
+		assertTrue(whiteboard.compareTo(beamer) > 0);
+		assertTrue(beamer.compareTo(whiteboard) < 0);
 	}
 
 }
