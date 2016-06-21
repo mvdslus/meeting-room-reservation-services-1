7864750f0ab9a7d9b53159a6379fb160823ce222
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-06-21 12:51:49 +0200

    Implement cucumber steps

--- src/it/java/com/github/verhagen/mrrs/it/cucumber/RoomSteps.java
+++ src/it/java/com/github/verhagen/mrrs/it/cucumber/RoomSteps.java
@@ -1,6 +1,37 @@
 package com.github.verhagen.mrrs.it.cucumber;
 
+import com.github.verhagen.mrrs.domain.Room;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class RoomSteps {
+	private Room room;
+
+
+	@Given("^a room with name \"(.*?)\", location \"(.*?)\" and capacity (\\d+)$")
+	public void a_room_with_name_Berlin_location_and_capacity(String name, String location, int capacity) throws Throwable {
+		room = new Room(name, location, capacity);
+	}
+
+	@Given("^which has facility \"(.*?)\"$")
+	public void which_has_facility(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^searching for room with location \"(.*?)\"$")
+	public void searching_for_room_with_location(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the room with name \"(.*?)\" should be returned$")
+	public void the_room_with_name_should_be_returned(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}	
 
 }
--- src/it/resources/room.feature
+++ src/it/resources/room.feature
@@ -2,7 +2,7 @@ Feature: Rooms require a location and capacity. Optional they have a name and fa
 
 
 Scenario: Room can be found by location
-Given a room with name "Berlin", location "1.12" and capacity 12 
+Given a room with name London, location "1.12" and capacity 12 
 	And which has facility "whiteboard"
 When searching for room with location "1.12"
 Then the room with name "Berlin" should be returned
