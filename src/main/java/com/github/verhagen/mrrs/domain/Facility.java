d3f0dc0fd7c8a5d27bb74738f951ee7b89d8e30f
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-06-24 20:56:40 +0200

    Adds argument validation

--- src/main/java/com/github/verhagen/mrrs/domain/Facility.java
+++ src/main/java/com/github/verhagen/mrrs/domain/Facility.java
@@ -1,16 +1,50 @@
 package com.github.verhagen.mrrs.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Facility {
 	private final String name;
 
 
 	public Facility(final String name) {
-		this.name = name;
+		final String nameCln = StringUtils.trimToNull(name);
+		if (nameCln == null) {
+			throw new IllegalArgumentException("Argument 'name' should not be null.");
+		}
+
+		this.name = nameCln;
 	}
 
 
 	public String getName() {
 		return name;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Facility other = (Facility) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
