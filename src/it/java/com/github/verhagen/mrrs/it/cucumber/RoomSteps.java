7314fb965ee272dc6f32b824a751e57bf7ab2206
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-06-24 20:57:00 +0200

    Implements scenario 'Room can be found by location with facilities'

--- src/it/java/com/github/verhagen/mrrs/it/cucumber/RoomSteps.java
+++ src/it/java/com/github/verhagen/mrrs/it/cucumber/RoomSteps.java
@@ -1,37 +1,65 @@
 package com.github.verhagen.mrrs.it.cucumber;
 
-import com.github.verhagen.mrrs.domain.Room;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import cucumber.api.PendingException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import com.github.verhagen.mrrs.domain.Facility;
+import com.github.verhagen.mrrs.domain.Room;
+import com.github.verhagen.mrrs.repository.RoomRepository;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class RoomSteps {
-	private Room room;
+	private RoomRepository repo = new RoomRepository();
+	private Room currentRoom;
 
 
 	@Given("^a room with name \"(.*?)\", location \"(.*?)\" and capacity (\\d+)$")
 	public void a_room_with_name_Berlin_location_and_capacity(String name, String location, int capacity) throws Throwable {
-		room = new Room(name, location, capacity);
+		repo.add(new Room(name, location, capacity));
 	}
 
-	@Given("^which has facility \"(.*?)\"$")
-	public void which_has_facility(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Given("^a room with name \"(.*?)\", location \"(.*?)\" and capacity (\\d+) with facilities: \"(.*?)\"$")
+	public void a_room_with_name_location_and_capacity_with_facilities(final String name, final String location,
+			final int capacity, final String facilitiesStr) throws Throwable {
+		final Set<Facility> facilities = new HashSet<>();
+		StringTokenizer tokenizer = new StringTokenizer(facilitiesStr, ",");
+		while (tokenizer.hasMoreTokens()) {
+			facilities.add(new Facility(tokenizer.nextToken()));
+		}
+
+		repo.add(new Room(name, location, capacity, facilities));
 	}
 
 	@When("^searching for room with location \"(.*?)\"$")
-	public void searching_for_room_with_location(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void searching_for_room_with_location(String location) throws Throwable {
+		currentRoom = repo.getByLocation(location);
 	}
 
 	@Then("^the room with name \"(.*?)\" should be returned$")
-	public void the_room_with_name_should_be_returned(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void the_room_with_name_should_be_returned(String name) throws Throwable {
+		assertEquals(name, currentRoom.getName());
 	}	
+
+	@Then("^the room should have facility \"(.*?)\"$")
+	public void the_room_should_have_facility(String name) throws Throwable {
+		assertTrue(currentRoom.getFacilities().contains(new Facility(name)));
+	}
+
+	private boolean containsFacility(Set<Facility> facilities, String name) {
+		Facility lookupFacility = new Facility(name);
+		for (Facility facility : facilities) {
+			if (facility.equals(lookupFacility)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
