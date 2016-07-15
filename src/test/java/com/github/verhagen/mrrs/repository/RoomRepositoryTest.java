b40682cb672c95b0573bfd31effbfff786b492f4
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-07-15 10:39:40 +0200

    Adds argument validation

--- src/test/java/com/github/verhagen/mrrs/repository/RoomRepositoryTest.java
+++ src/test/java/com/github/verhagen/mrrs/repository/RoomRepositoryTest.java
@@ -2,14 +2,20 @@ package com.github.verhagen.mrrs.repository;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.core.IsEqual;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.github.verhagen.mrrs.domain.Room;
 
 public class RoomRepositoryTest {
+	@Rule
+	public ExpectedException expExcep = ExpectedException.none();
+
 
 	@Test
-	public void testName() throws Exception {
+	public void getByLocation() throws Exception {
 		RoomRepository repo = new RoomRepository();
 		repo.add(new Room("01.12", 8));
 		repo.add(new Room("01.14", 8));
@@ -19,7 +25,25 @@ public class RoomRepositoryTest {
 
 		Room room = repo.getByLocation("01.14");
 		assertNotNull(room);
-		assertEquals(room.getCapacity(), 12);
+		assertEquals(room.getCapacity(), 8);
+	}
+
+	@Test	
+	public void getByLocationNull() throws Exception {
+		expExcep.expect(IllegalArgumentException.class);
+		expExcep.expectMessage(new IsEqual<String>("Argument 'location' should not be null."));
+
+		RoomRepository repo = new RoomRepository();
+		repo.getByLocation("\t");
+	}
+
+	@Test	
+	public void getByLocationNotKnown() throws Exception {
+		expExcep.expect(IllegalArgumentException.class);
+		expExcep.expectMessage(new IsEqual<String>("Argument 'location' with value '01.10' is not a known room."));
+
+		RoomRepository repo = new RoomRepository();
+		repo.getByLocation("01.10");
 	}
 
 }
