141d702eb12c6564a9204ad66518e5560a243b0c
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-07-18 15:07:39 +0200

    Implements CsvRoomRepository, through CSVeed.org library

--- src/test/java/com/github/verhagen/mrrs/repository/csv/CsvRoomRepositoryTest.java
+++ src/test/java/com/github/verhagen/mrrs/repository/csv/CsvRoomRepositoryTest.java
@@ -15,14 +15,21 @@ public class CsvRoomRepositoryTest {
 	public void create() throws Exception {
 		RoomRepository roomRepo = CsvRoomRepository.importBy(createReader());
 
+		assertEquals("A02", roomRepo.getByLocation("A02").getName());
+		assertEquals("A02, 6", roomRepo.getByLocation("A02").toString());
+
 		assertEquals("Amsterdam", roomRepo.getByLocation("B22").getName());
+		assertEquals("Amsterdam, B22, 10, [ white board ]", roomRepo.getByLocation("B22").toString());
+
 		assertEquals("Berlin", roomRepo.getByLocation("B24").getName());
+		assertEquals("Berlin, B24, 16, [ large screen, white board ]", roomRepo.getByLocation("B24").toString());
 	}
 
 
 	private Reader createReader() {
 		final StringBuilder bldr = new StringBuilder();
 		bldr.append("name; location; capacity; facilities\n");
+		bldr.append("; A02; 6;\n");
 		bldr.append("Amsterdam; B22; 10; white board\n");
 		bldr.append("Berlin; B24; 16; large screen, white board\n");
 		return new StringReader(bldr.toString());
