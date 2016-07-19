9ff1d8e0781c31dd5605825bafef43daf8ffbd17
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-07-19 11:58:37 +0200

    Updates room feature, with direct entries for facilities Adds first implementation of the steps, renames method arguments

--- src/it/java/com/github/verhagen/mrrs/it/cucumber/RoomSteps.java
+++ src/it/java/com/github/verhagen/mrrs/it/cucumber/RoomSteps.java
@@ -1,6 +1,52 @@
 package com.github.verhagen.mrrs.it.cucumber;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class RoomSteps {
+
+	@Given("^a room with name '(.*?)', location '(.*?)' and capacity (\\d+)$")
+	public void a_room_with_name_location_and_capacity(final String name, final String location, final int capacity) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	@Given("^a room with name '(.*?)', location '(.*?)' and capacity (\\d+) which has facilities '(.*?)'$")
+	public void a_room_with_name_Berlin_location_and_capacity_which_has_facilities_whiteboard(final String name, final String location,
+			final int capacity, final String facilities) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^which has facility '(.*?)'$")
+	public void which_has_facility(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^searching for room with location '(.*?)'$")
+	public void searching_for_room_with_location(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the room with name '(.*?)' should be returned$")
+	public void the_room_with_name_should_be_returned(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^no facilities$")
+	public void no_facilities() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^has facilities '(.*?)'$")
+	public void has_facilities_whiteboard() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
 }
--- src/it/resources/room.feature
+++ src/it/resources/room.feature
@@ -1,8 +1,14 @@
 Feature: Rooms require a location and capacity. Optional they have a name and facilities.
 
 
-Scenario: Room can be found by location
-Given a room with name "Berlin", location "1.12" and capacity 12 
-	And which has facility "whiteboard"
-When searching for room with location "1.12"
-Then the room with name "Berlin" should be returned
+Scenario: Room is found by location
+Given a room with name 'Berlin', location '1.12' and capacity 12 
+When searching for room with location '1.12'
+Then the room with name 'Berlin' should be returned
+  And no facilities 
+
+Scenario: Room is found found by location
+Given a room with name 'Berlin', location '1.12' and capacity 12 which has facilities 'whiteboard'
+When searching for room with location '1.12'
+Then the room with name 'Berlin' should be returned
+  And has facilities 'whiteboard'
